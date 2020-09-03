ed2166d7ae50cf7f8d6416fba16ac907133317eb
Denis <ferrany1@protonmail.ch>
2020-09-03 13:52:18 +0300

    Commit

--- WeeklyBinanceBuyBot.go
+++ WeeklyBinanceBuyBot.go
@@ -3,8 +3,16 @@ package main
 import (
 	"WeeklyBinanceBuyBot_Go/lib/LRequest"
 	"github.com/aws/aws-lambda-go/lambda"
+	"os"
 )
 
 func main() {
-	lambda.Start(LRequest.HandleRequest)
+
+	switch OS := os.Getenv("OS"); {
+	case OS == "Lambda":
+		lambda.Start(LRequest.HandleRequest)
+	default:
+		LRequest.HandleNonApiRequest()
+	}
+
 }
